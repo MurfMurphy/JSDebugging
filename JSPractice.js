2b14d4cc4775c03c717ae178cd2da4e3113cd9df
Alec.Murphy <alecmurphypersonal@gmail.com>
2018-07-31 20:59:53 -0400

    homework basic javascript problems Murphy

---
+++ JSPractice.js
@@ -0,0 +1,55 @@
+// //Problem 1
+// var math1 = 10
+//     math2 = 15
+
+// console.log(math1 + math2);
+
+// //Problem 2
+// var dogs = ["Golden Retriever", "Malamute", "German Shephard"]
+//     cats = ["Tortoiseshell", "Tabby", "Scottish Fold"]
+//     animals = [dogs, cats]
+
+// console.log(animals[0][2]);
+// console.log(animals[1][0]);
+
+// //Problem 3
+// var numbercheck = prompt ('Enter a value under 100');
+//     if(numbercheck<100){
+//         alert('THAT NUMBER IS TOO SMALL')
+//     } else if(numbercheck>100){
+//         alert('YOUR NUMBER, ' + numbercheck + ', IS GREATER THAN 100')
+//     }
+
+// //Problem 5
+// var mystate = prompt ('Pick my current state');
+//     if(mystate=='Despair'){
+//         alert('Wow nice freakin guess')
+//     } else if(mystate!='Despair'){
+//         alert("HOW DIDN'T YOU GET THAT YOU'RE SO STUPID")
+//     }
+
+// //Problem 6
+
+// function yourNameIs(yourName){
+//     var yourName = prompt ('Tell me your name');
+//     return alert('Your name is ' + yourName);
+// }
+// Yournameis();
+
+// // Problem 7
+// function notDeclared(){
+//     console.log('Hey nice undeclared function my guy')
+// }
+// notDeclared();
+
+// //Problem 8
+// function mysterydoor(door){
+//     if (door === 1){
+//         return alert('YOU HAVE CHOSEN, POORLY')
+//     } else if (door === 2){
+//         return alert('OH YOU GET THE PRIZE!!!!')
+//     } else if (door === 3){
+//         return alert('Congratulations, you are the 1,000,000 visitor, input name and SSN to win a prize')
+//     }
+// }
+// mysterydoor(3);
